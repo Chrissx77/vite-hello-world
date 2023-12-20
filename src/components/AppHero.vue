65d6c5f5f3547631266b5b44bcb541d25e58b620
ChrisGarozzo <garozzochristian@gmail.com>
2023-12-20 16:55:49 +0100

    added calcutta image

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+  name: "AppHero"
+}
+
+</script>
+
+<template>
+  <div class="jumbotron">
+    <img src="../assets/calcutta.jpeg" alt="" srcset="">
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: red;
+}
+
+
+</style>
